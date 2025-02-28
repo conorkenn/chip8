b897854303825bdf13cb0725b356db36752a541e
Conor Kennedy <conor@conorkennedy.com>
2025-02-28 18:20:50 -0500

    added dxyn for outputing

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 type Chip8 struct {
 	memory  [4096]byte
 	display [64][32]bool // 64 x 32 display
-	VF      [16]byte     // 16 8-bit registers
 	PC      uint16       // program counter
 	I       uint16       // index register
 	stack   [16]uint16   // stack for subroutines
@@ -84,7 +83,7 @@ func (c *Chip8) Fetch() uint16 {
 
 func (c *Chip8) Execute(opcode uint16) {
 	switch opcode & 0xF000 {
-	case 0x000:
+	case 0x0000:
 		switch opcode {
 		case 0x00E0: // clear
 			for x := range c.display {
@@ -110,6 +109,26 @@ func (c *Chip8) Execute(opcode uint16) {
 	case 0x7000: // add to vx
 		x := (opcode & 0x0F00) >> 8
 		c.V[x] += byte(opcode & 0x00FF)
+	case 0xD000: //dxyn
+		x := c.V[(opcode&0x0F00)>>8]
+		y := c.V[(opcode&0x00F0)>>4]
+		height := opcode & 0x000F
+		c.V[0xF] = 0
+
+		for row := uint16(0); row < height; row++ {
+			spriteByte := c.memory[c.I+row]
+			for col := uint8(0); col < 8; col++ {
+				if (spriteByte & (0x80 >> col)) != 0 {
+					xPos := (x + col) % 64
+					yPos := (y + uint8(row)) % 32
+					current := c.display[xPos][yPos]
+					c.display[xPos][yPos] = current != true
+					if current && !c.display[xPos][yPos] {
+						c.V[0xF] = 1
+					}
+				}
+			}
+		}
 	default:
 		fmt.Printf("Unkown opcode: %04X\n", opcode)
 	}
@@ -118,6 +137,20 @@ func (c *Chip8) Execute(opcode uint16) {
 func (c *Chip8) Cycle() {
 	opcode := c.Fetch()
 	c.Execute(opcode)
+}
+
+func (c *Chip8) PrintDisplay() {
+	for y := 0; y < 32; y++ {
+		for x := 0; x < 64; x++ {
+			if c.display[x][y] {
+				fmt.Print("█")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
+	fmt.Println("---")
 }
 
 func main() {
@@ -135,9 +168,11 @@ func main() {
 
 	for i := 0; i < 1000; i++ {
 		emulator.Cycle()
-		time.Sleep(500 * time.Millisecond)
-		fmt.Printf("DT: %d, ST: %d\n", emulator.DT, emulator.ST)
-		fmt.Printf("Cycle %d: PC=%04X, V0=%02X\n", i, emulator.PC, emulator.V[0])
+		time.Sleep(2 * time.Millisecond) // ~500 Hz
+		if i%100 == 0 {
+			fmt.Printf("Cycle %d: PC=%04X, V0=%02X\n", i, emulator.PC, emulator.V[0])
+			emulator.PrintDisplay()
+		}
 	}
 
 }
